493188321b32880fcd3a33615c5b9fc986882197
Serban <sharebaanda@gmail.com>
2020-06-03 18:10:26 +0300

    namespace fix

--- src/routes/phobos.php
+++ src/routes/phobos.php
@@ -37,7 +37,7 @@ Route::middleware('auth:api')->namespace('Phobos\Framework\app\Http\Controllers'
 });
 
 Route::middleware('auth:api')->group(function() {
-    Route::post('logout', 'Auth\LoginController@apiLogout');
+    Route::post('logout', 'App\Http\Controllers\Auth\LoginController@apiLogout');
 });
 
 Route::get('config', 'AppController@config');
