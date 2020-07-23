3ad9ad3ccee6af79a148db19538bcc632ecc3ba3
Serban <sharebaanda@gmail.com>
2020-07-23 18:12:54 +0300

    auth model namespace change

--- src/config/auth.php
+++ src/config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\PhobosUser::class,
+            'model' => Phobos\Framework\App\Models\PhobosUser::class,
         ],
 
         // 'users' => [
